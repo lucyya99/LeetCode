aef26b500f52942100a935bf6de34922c5cd1f02
Yeji Kim <80736490+lucyya99@users.noreply.github.com>
2024-06-26 09:58:33 +0900

    Time: 38 ms (62.25%), Space: 16.4 MB (88.84%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.py
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.py
@@ -3,10 +3,5 @@ class Solution:
         # num1 : [1, n] 중에 m으로 안나눠지는거의 합
         # num2 : [1, n] 중에 m으로 나눠지는거의 합
         # num1 - num2
-        result = 0
-        for i in range(1, n+1):
-            if i % m == 0:
-                result -= i
-            else:
-                result += i
-        return result
+        result = [-i if i % m == 0 else i for i in range(1, n+1)]
+        return sum(result)
